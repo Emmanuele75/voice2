999742f2feb025ae1b2d30659c8cbf5a0d7f70f3
Maury <128136690+Emmanuele75@users.noreply.github.com>
2025-05-28 16:00:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 //const WS_PORT = process.env.WS_PORT || 8888; //8888
 //const HTTP_PORT = process.env.HTTP_PORT || 8000; //8000
 
-const WS_PORT = process.env.PORT || 8888; //8888
+const PORT = process.env.PORT || 8888; //8888
 const HTTP_PORT = process.env.PORT || 8000; //8000
 
 
-const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
-  console.log(`WS server is listening at ws://localhost:${WS_PORT}`)
+const wsServer = new WebSocket.Server({ port: PORT }, () =>
+  console.log(`WS server is listening at ws://localhost:${PORT}`)
 );
 
 // array of connected websocket clients
