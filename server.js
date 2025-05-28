a09d2b884a1cd23d6434c9ababd7d8ec91cc7499
Maury <128136690+Emmanuele75@users.noreply.github.com>
2025-05-28 14:18:06 +0200

    d
    d

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const WebSocket = require("ws");
 const app = express();
 
-const WS_PORT = process.env.WS_PORT || 8080; //8888
-const HTTP_PORT = process.env.HTTP_PORT || 80; //8000
+const WS_PORT = process.env.WS_PORT || 80; //8888
+const HTTP_PORT = process.env.HTTP_PORT || 8080; //8000
 
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
   console.log(`WS server is listening at ws://localhost:${WS_PORT}`)
