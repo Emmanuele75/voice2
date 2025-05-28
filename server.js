3f7ca5d862f1e3590f4df22d697445b18e6fed1f
Maury <128136690+Emmanuele75@users.noreply.github.com>
2025-05-28 16:31:21 +0200

    w
    w

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const WebSocket = require("ws");
 const app = express();
 
-const WS_PORT = process.env.WS_PORT || 443; //8888
-const HTTP_PORT = process.env.HTTP_PORT || 80; //8000
+const WS_PORT = process.env.WS_PORT || 80;
+const HTTP_PORT = process.env.HTTP_PORT || 443; 
 
 
 const wsServer = new WebSocket.Server({ port: WS_PORT }, () =>
